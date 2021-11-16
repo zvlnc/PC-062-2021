88bfc9cc58af9175e68b1a937a7b32b8d55439e3
Alex <89739785+zvlnc@users.noreply.github.com>
2021-11-15 23:31:10 -0600

    Script E15 Añadido.

---
+++ Python/E15/E15.py
@@ -0,0 +1,58 @@
+import pyautogui,time
+
+
+pyautogui.click(x=, y =, clicks = 2)
+pyautogui.press('tab')
+pyautogui.press('tab')
+time.sleep(5)
+
+pyautogui.typewrite(" ")
+pyautogui.press('tab')
+pyautogui.press('tab')
+time.sleep(5)
+
+pyautogui.press('enter')
+pyautogui.click(x=, y =, clicks = 1)
+pyautogui.press('tab')
+pyautogui.press('tab')
+time.sleep(5)
+
+pyautogui.typewrite(" ")
+pyautogui.hotkey('altright','q')
+pyautogui.typewrite(" ")
+pyautogui.press('tab')
+time.sleep(5)
+
+pyautogui.press('enter')
+
+time.sleep(10)
+
+
+pyautogui.click(x=, y =, clicks = 2)
+    
+
+pyautogui.click(x=, y =, clicks = 2)
+pyautogui.press('tab')
+pyautogui.press('tab')
+time.sleep(5)
+
+pyautogui.typewrite("")
+pyautogui.press('tab')
+pyautogui.press('tab')
+time.sleep(5)
+
+pyautogui.press('enter')
+
+pyautogui.click(x=, y =, clicks = 1)
+pyautogui.press('tab')
+pyautogui.press('tab')
+time.sleep(5)
+
+pyautogui.typewrite("")
+pyautogui.hotkey('altright','q')
+pyautogui.typewrite("")
+pyautogui.press('tab')
+time.sleep(5)
+
+pyautogui.press('enter')
+
